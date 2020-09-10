093ebc4ba4b5df5ac02d2793e9534816bf3e0c73
MirnaCardona <69806001+MirnaCardona@users.noreply.github.com>
2020-09-10 16:43:49 -0500

    Update files.c

--- files.c
+++ files.c
@@ -27,6 +27,6 @@ printf("El programa termino de manera anormal");
 exit(0);}
 else
 for(i=0;i<rows;i++)
-  fprintf(file, "%d\t%Le\t%Le",  buff[i][i], buff[i][RECURSIVE], buff[i][ITERATIVE]);
+  fprintf(file, "%Le\t%Le\t%Le",  buff[i][i], buff[i][RECURSIVE], buff[i][ITERATIVE]);
   
 }
