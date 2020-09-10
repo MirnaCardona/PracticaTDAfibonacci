764e7c573f61f4a90b809437847365a2b7989dca
MirnaCardona <69806001+MirnaCardona@users.noreply.github.com>
2020-09-10 16:34:25 -0500

    Update files.c

--- files.c
+++ files.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "files.h"
+#include "FibMain.h" 
 
 /**
  * Writes a bidimensional buffer array into a file.
@@ -18,14 +19,14 @@
 */
 
 void file_num_write(FILE * file, size_t columns, long double buff[] [columns], size_t rows)
-{
+{int i;
 if(file==NULL)
 {
 printf("Error: No fue posible gnerar el archivo, revise sus unidades de almacenamiento");
 printf("El programa termino de manera anormal");
 exit(0);}
 else
-for(i=0;i<size_t_rows;i++)
+for(i=0;i<rows;i++)
   fprintf(file, "%d\t%d\t%d",  buff[i][index], buff[i][RECURSIVE], buff[i][ITERATIVE]);
   
 }
